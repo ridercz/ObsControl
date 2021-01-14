a187092fb6ac4e7eb3cad2aab4bc535c14ce1a00
Michal Altair Valášek <michal.valasek@altairis.cz>
2021-01-14 21:37:55 +0100

    Add startup blinking LEDs.

--- ObsControlFirmware/ObsControlFirmware.ino
+++ ObsControlFirmware/ObsControlFirmware.ino
@@ -1,26 +1,44 @@
+/* 
+ * OBS STUDIO CONTROL RIG version 1.5 (2021-01-14)
+ * 
+ * Copyright (c) Michal Altair Valasek, 2019-2021
+ * Licensed under terms of the MIT License.
+ * 
+ * https://www.rider.cz
+ * https://www.altair.blog
+ * https://github.com/ridercz/ObsControl/
+ * 
+ */
+
 #include <Pushbutton.h>
 #include <Keyboard.h>
 
 // Uncomment the following line to print values to serial instead of sending keypresses
 // #define WHATIF
-#define KEYPRESS_LENGTH         50
+
+// How long should the key be down?
+#define KEYPRESS_LENGTH         50 // ms
+
+// POST LED blinking
+#define TEST_LED_COUNT          5
+#define TEST_LED_SPEED          100 // ms
 
 // Define pins and count for standard buttons
 #define STANDARD_BUTTON_COUNT   5
 Pushbutton standardButtons[] {
-  Pushbutton(6),                // Green
-  Pushbutton(5),                // Yellow
-  Pushbutton(4),                // Black
-  Pushbutton(3),                // Blue
-  Pushbutton(2),                // Red
+  Pushbutton(6),                // Green  (Ctrl-Shift-Alt-F1)
+  Pushbutton(5),                // Yellow (Ctrl-Shift-Alt-F2)
+  Pushbutton(4),                // Black  (Ctrl-Shift-Alt-F3)
+  Pushbutton(3),                // Blue   (Ctrl-Shift-Alt-F4)
+  Pushbutton(2),                // Red    (Ctrl-Shift-Alt-F5)
 };
 
 // Define pins and count for LED status buttons
 #define STATUS_BUTTON_COUNT     2
 #define STATUS_BUTTON_LED       14
 Pushbutton statusButtons[] {
-  Pushbutton(8),                // Square red
-  Pushbutton(7),                // Square green
+  Pushbutton(8),                // Square red   (Ctrl-Shift-Alt-F6)
+  Pushbutton(7),                // Square green (Ctrl-Shift-Alt-F7)
 };
 bool statusCurrent[STATUS_BUTTON_COUNT];
 
@@ -36,7 +54,12 @@ void setup() {
   for (int i = 0; i < STATUS_BUTTON_COUNT; i++) {
     statusCurrent[i] = false;
     pinMode(STATUS_BUTTON_LED + i, OUTPUT);
-    digitalWrite(STATUS_BUTTON_LED + i, false);
+    for(int j = 0; j < TEST_LED_COUNT; j++) {
+      digitalWrite(STATUS_BUTTON_LED + i, HIGH);
+      delay(TEST_LED_SPEED);
+      digitalWrite(STATUS_BUTTON_LED + i, LOW);
+      delay(TEST_LED_SPEED);
+    }
   }
 
 #ifdef WHATIF
